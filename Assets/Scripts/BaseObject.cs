df8414f3af96ecf2d1ebc4d55f7106968ceb50ed
Sagiv Ofek <sagiv4@gmail.com>
2016-01-30 10:12:53 -0500

    Kevin!

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void l(object o){
+		Debug.Log (o);
+	}
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : BaseObject {
+
+	public float needActionEvery = 3f;
+
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("askAction", needActionEvery, needActionEvery);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void askAction(){
+		l ("action now!");
+	}
+}
